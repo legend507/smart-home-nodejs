00d0db43cd8a976629875e847d038e6cedcb0f0f
legend507 <chenfeiyu507@gmail.com>
2018-05-11 14:30:59 +0900

    followed the instructions, created credential key and modified config-provider.js

--- smart-home-provider/cloud/config-provider.js
+++ smart-home-provider/cloud/config-provider.js
@@ -22,7 +22,7 @@ Config.smartHomeProviderGoogleClientId = 'ZxjqWpsYj3';
 // Client secret that Google will use
 Config.smartHomeProvideGoogleClientSecret = 'hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv';
 // Client API Key generated on the console
-Config.smartHomeProviderApiKey = '<API_KEY>';
+Config.smartHomeProviderApiKey = 'AIzaSyAE9mXbH39KnO75DNuNGLp6L81W_EbJ_GQ';
 // Client service key to use for reporting state
 Config.jwt = require('./jwt-key.json');
 // Running server locally using ngrok
